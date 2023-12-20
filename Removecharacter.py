e530e107f0c7891780dff2413190e21823fdee79
Kate Jue Wang <wangjuekate@gmail.com>
2023-12-20 12:49:08 -0500

    Update Removecharacter.py

--- Removecharacter.py
+++ Removecharacter.py
@@ -1,1 +1,5 @@
 int(''.join(filter(str.isdigit, '200 grams 5')))
+privatefirminvestor3['vrdamtsume_test'] = privatefirminvestor3['vrdamtsume'].str.replace(',', '', regex=True)
+privatefirminvestor3.loc[(privatefirminvestor3['vrdamtsume_test']==''),'vrdamtsume_test'] = '0'
+privatefirminvestor3['vrdamtsume_test'] = privatefirminvestor3['vrdamtsume_test'].astype(float)
+
