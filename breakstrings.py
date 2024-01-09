1e7d8119fac1d95f14266bb9cd0e06257d06033f
Kate Jue Wang <wangjuekate@gmail.com>
2024-01-09 11:53:27 -0500

    Update breakstrings.py

--- breakstrings.py
+++ breakstrings.py
@@ -1,1 +1,7 @@
 applicationinfo[['first','second']]=applicationinfo['application_id'].str.split(pat ='/', expand = True)
+
+df3 = protests['date'].str.split(pat ='-', expand=True)
+df3.columns = ['STATUS_ID{}'.format(x+1) for x in df3.columns]
+
+protests[['year','month','day'] ]=df3[['STATUS_ID1','STATUS_ID2', 'STATUS_ID3']]
+
